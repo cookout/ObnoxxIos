c962fa62547fd927458db6c4f5b767aaa114a704
craynium <cra@Chandrashekars-MacBook-Air.local>
2014-07-30 11:53:32 -0700

    Initial Commit

---
+++ Obnoxx/OBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  OBAppDelegate.h
+//  Obnoxx
+//
+//  Created by Chandrashekar Raghavan on 7/30/14.
+//  Copyright (c) 2014 Obnoxx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Obnoxx/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Obnoxx
+//
+//  Created by Chandrashekar Raghavan on 7/30/14.
+//  Copyright (c) 2014 Obnoxx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "OBAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([OBAppDelegate class]));
+    }
+}
